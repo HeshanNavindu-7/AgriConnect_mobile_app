5042b041aecfa42c2df3428e0d6b75085d36ea6a
heshan <121144605+HeshanNavindu-7@users.noreply.github.com>
2023-10-02 14:22:59 +0530

    file

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import CustomLebel from './src/components/CustomLebel';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+<CustomLebel></CustomLebel>
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/components/CustomLebel.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const CustomLebel = () => {
+  return (
+    <View>
+      <Text>CustomLebel</Text>
+    </View>
+  )
+}
+
+export default CustomLebel;
