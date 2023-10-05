a5e5f590201e0f1d850ab6a7e0a1d78926d5c33d
heshan <121144605+HeshanNavindu-7@users.noreply.github.com>
2023-10-05 23:32:58 +0530

    navigation

---
+++ src/components/Screens/Home1.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Ionicons } from '@expo/vector-icons'; 
+const Home1 = () => {
+  return (
+    <View>
+      <Text>Home</Text>
+      <Ionicons name="home-outline" size={24} color="black" />
+    </View>
+  )
+}
+
+export default Home1 ;
