4237f209e23f00a0309e8d94468dfa1aa9c88ccd
D_Binara <dbkaveesha@gmail.com>
2023-10-05 20:21:23 +0530

    add FirstPage

--- App.js
+++ App.js
@@ -3,10 +3,11 @@ import { StyleSheet, Text, View } from 'react-native';
 import CustomLebel from './src/components/CustomLebel';
 import Signin from './src/components/Sign_In';
 import Signup from './src/components/Sign_Up';
+import FirstPage from './src/components/FirstPage';
 
 export default function App() {
   return (
-    <Signup/>
+    <FirstPage/>
   );
 }
 
