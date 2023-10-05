c342e1e51c5b800b05977091b585573c3aaf4fd8
D_Binara <dbkaveesha@gmail.com>
2023-10-05 20:43:09 +0530

    add SecondPage

--- App.js
+++ App.js
@@ -4,10 +4,11 @@ import CustomLebel from './src/components/CustomLebel';
 import Signin from './src/components/Sign_In';
 import Signup from './src/components/Sign_Up';
 import FirstPage from './src/components/FirstPage';
+import SecondPage from './src/components/SecondPage';
 
 export default function App() {
   return (
-    <FirstPage/>
+    <SecondPage/>
   );
 }
 
